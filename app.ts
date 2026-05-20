da832f1a1f0010657f6f2e7474d8756729c69d6e
Charles Zawacki <cazwacki@vt.edu>
2026-05-20 13:16:30 -0400

    feat: change appearance based on system theme

--- app.ts
+++ app.ts
@@ -1,11 +1,29 @@
 import app from "ags/gtk4/app"
-import style from "./style.scss"
+import style_dark from "./style-dark.scss"
+import style_light from "./style-light.scss"
 import Bar from "./widget/bar/Bar"
 import NotificationPopup from "./widget/notification/NotificationPopup"
+import Gio from "gi://Gio"
 
+
+const user_interface = Gio.Settings.new('org.gnome.desktop.interface');
+// Function to update the style based on the current scheme
+function updateTheme() {
+    const isDark = user_interface.get_string('color-scheme') === 'prefer-dark';
+    
+    // Swap your CSS file based on the preference
+    const cssFile = isDark ? style_dark : style_light;
+    app.apply_css(cssFile, true);
+}
+
+// Listen for changes
+user_interface.connect('changed::color-scheme', updateTheme);
+
+// Initial theme update
+updateTheme();
 
 app.start({
-  css: style,
+  css: style_dark,
   // gtkTheme: "adw-gtk3-dark",
   main() {
     app.get_monitors().map(Bar)
